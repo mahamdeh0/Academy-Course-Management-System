02726acd6ae891f5490c8282752659dcf77ab5bc
mahamdeh0 <mahamdehabd@gmail.com>
2024-06-05 16:07:38 +0300

    Add project files.

---
+++ Data/Config/EnrollmentConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using EFCoreExample.CodeFirst.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCoreExample.Data.Config
+{
+    public class EnrollmentConfiguration : IEntityTypeConfiguration<Enrollment>
+    {
+        public void Configure(EntityTypeBuilder<Enrollment> builder)
+        {
+            builder.HasKey(x => new { x.SectionId, x.StudentId });
+
+
+
+            builder.ToTable("Enrollments");
+
+
+            builder.HasData(LoadEnrollments());
+        }
+
+        private static List<Enrollment> LoadEnrollments()
+        {
+            return new List<Enrollment>
+            {
+                new Enrollment() { StudentId = 1, SectionId = 6 },
+                new Enrollment() { StudentId = 2, SectionId = 6 },
+                new Enrollment() { StudentId = 3, SectionId = 7 },
+                new Enrollment() { StudentId = 4, SectionId = 7 },
+                new Enrollment() { StudentId = 5, SectionId = 8 },
+                new Enrollment() { StudentId = 6, SectionId = 8 },
+                new Enrollment() { StudentId = 7, SectionId = 9 },
+                new Enrollment() { StudentId = 8, SectionId = 9 },
+                new Enrollment() { StudentId = 9, SectionId = 10 },
+                new Enrollment() { StudentId = 10, SectionId = 10 }
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using EFCoreExample.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreExample.CodeFirst
+{
+    class Program
+    {    
+        public static void Main(string[] args)
+        {
+            using (var context = new AppDbContext())
+            {
+                foreach (var item in context.Sections.Include(x => x.Course))
+                {
+                    Console.WriteLine($"Section: {item.SectionName}, " +
+                        $"Course {item.Course.CourseName}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
